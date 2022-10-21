46c6e4a76afc5b931aa91cd53395e2bec9a89a74
Alejandro Socarras <apsocarras@gmail.com>
2022-10-21 15:05:29 -0600

    Minimize branching with nested loop over object, save ascripts-alt.js

--- scripts-alt.js
+++ scripts-alt.js
@@ -1,3 +1,10 @@
+// This is an alternative .js file that works with the site and provides the same functionality as scripts.js.
+// Instead of branching, it nests a second for-loop within the first that iterates over an object called "suggestionKey", 
+// which replaces the individual variables "python", "r", and "assembly".
+// Does one if statement without an else count as branching? I wasn't sure.
+// Normally I would submit this alternative in its own branch, but I know that for grading purposes only `main` is checked on this assignment. 
+// So I saved it here to keep record of the work I've put into it.
+
 function clearPreviousResult () {
   document.getElementById("python").setAttribute("class", "hidden");
   document.getElementById("r").setAttribute("class", "hidden");
@@ -14,27 +21,26 @@ function suggestLang() {
   const answer6 = document.querySelector("input[name='question6']:checked");
   const answer7 = document.querySelector("input[name='question7']:checked");
 
-  let answers = [answer1,answer2,answer3,answer4,answer5,answer6,answer7]
+  let answers = [answer1,answer2,answer3,answer4,answer5,answer6,answer7];
 
-  // Old, basic branching method
-  let python = 0;
-  let r = 0;
-  let assembly = 0;
-
-  for (i = 0; i < answers.length; i++) {
-    if (answers[i].className === "python") {
-      python += 1;
-    } else if (answers[i].className === "r") {
-      r += 1;
-    } else if (answers[i].className === "assembly") {
-      assembly += 1;
-    }
+  let suggestionKey = {
+    python: 0,
+    r: 0,
+    assembly: 0
   }
 
-  let langNames = ["python","r","assembly"];
-  let tally = [python,r,assembly]; 
-  const index = tally.indexOf(Math.max(...tally));
-  const langSuggestion = langNames[index];
+  for (i = 0; i < answers.length; i++) {
+    for (let [key,value] of Object.entries(suggestionKey)) {
+      if (answers[i].className === key) {
+        suggestionKey[key] = value + 1; 
+      }
+    }
+  }
+  
+  const tallies = Object.values(suggestionKey);
+  const index = tallies.indexOf(Math.max(...tallies));
+  const langSuggestion = Object.keys(suggestionKey)[index];
+
   return langSuggestion;
 
 }
